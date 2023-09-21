f5505a9819a242bf8a560071c77597aeea895019
Jay <stani152@morris.umn.edu>
2023-09-21 10:47:48 -0500

    Actually finish lab
    I don't know why doing 'git commit' in the terminal committed the wrong version of my files... but whatever, hopefully the tests pass now

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -13,7 +13,7 @@ fi
 # The argument -d specifies the object we are making is a directory.
 # The argument -p allows us to specify a relative filepath for this directory (by default, it is made in /tmp)
 # We should also capture the output of mktemp in a variable so we can actually access its filepath later. Using parentheses () in the variable assignment allows us to do so.
-tempdir=$(mktemp -dp .)
+tempdir=$(mktemp -dt tmp.XXXXXXXX)
 
 # Store the current directory for use later.
 here=$(pwd)
@@ -24,22 +24,22 @@ tar -xzf "${1}" -C "${tempdir}"
 
 # Loop through files in this new extracted directory...
 # The % operator deletes the '.tgz' match from the name of the tar file we extracted, so we can access its directory within the tempdir... thanks to pfnuesel on StackOverflow for this one.
-cd "${tempdir}/${1%.tgz}"
+cd "${tempdir}/${1%.tgz}" || exit 1
 
 for file in *; do
 	# Send the output of the grep search for "DELETE ME!" in each file to a variable.
 	# There is probably a better way to do this... but as I'm just starting to get familiar with how these searching tools work, this option is the most intuitive to me for right now.
-	out=$(grep "DELETE ME!" file)
-	# The -z flag checks that the variable is empty; i.e., if grep does contain some output, delete the file
-	if [ ! -z "${out}" ]; then
-		rm file
+	out=$(grep "DELETE ME!" "${file}")
+	# The -n flag checks that the variable is empty; i.e., if grep does contain some output, delete the file
+	if [ -n "${out}" ]; then
+		rm "${file}"
 	fi
 done
 
 # Once we've cleaned the files, we make a new tar archive containing them.
 # We HAVE to be in the scratch directory to ensure that the file names are correct.
-cd ${tempdir}
-newtar=$(tar -zcf "cleaned_${1}" "cleaned_${1%.tgz}")
+cd "${tempdir}" || exit 1
+tar -zcf "cleaned_${1}" "${1%.tgz}"
 
 # Move the new tar file into cleaning/
-mv ${newtar} ${here}
+mv "cleaned_${1}" "${here}"
